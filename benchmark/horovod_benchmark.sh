90cdfc9762b960f7487f54df70cd4123d0c9fcbc
Yuxi Hu <darrenyxhu@gmail.com>
2019-01-10 19:41:32 -0800

    Add scritps to run horovod performance benchmark

---
+++ benchmark/horovod_benchmark.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+function run_benchmark() {
+    NUM_WORKERS=$1
+    NUM_HOSTS=$(expr $NUM_WORKERS / 8)
+    MODEL=$2
+    MODE=$3
+    ALLRECUDE=$4
+    DTYPE=$5
+    DATA_SRC=$6
+
+
+    if [[ $DTYPE = "float32" ]]; then
+        LR=0.05
+        BATCH_SIZE=128
+    else
+        LR=0.1
+        BATCH_SIZE=256
+    fi
+
+    ENV="-x NCCL_DEBUG=INFO -x NCCL_MIN_NRINGS=4 -x MXNET_USE_OPERATOR_TUNING=0 -x HOROVOD_STALL_CHECK_DISABLE=1"
+    if [[ $ALLRECUDE = "HA" ]]; then
+        ENV="$ENV -x HOROVOD_HIERARCHICAL_ALLREDUCE=1"
+    fi
+
+    OPTIONS="--model $MODEL --dtype $DTYPE --batch-size $BATCH_SIZE --lr $LR --lr-mode $MODE --warmup-epochs 1 --num-epochs 2 --data-nthreads 4 --last-gamma"
+    if [[ $DATA_SRC != "synthetic" ]]; then
+        OPTIONS="$OPTIONS --use-rec --rec-train /media/ramdisk/train-480px-q95.rec --rec-train-idx /media/ramdisk/train-480px-q95.idx --rec-val /media/ramdisk/val-480px-q95.rec --rec-val-idx /media/ramdisk/val-480px-q95.idx"
+    fi
+
+    MPIRUN="mpirun -np $NUM_WORKERS --hostfile $HOME/${NUM_HOSTS}hosts --bind-to none --map-by slot -mca pml ob1 -mca btl ^openib"
+    CMD="/home/ubuntu/.virtualenvs/mxnet/bin/python /home/ubuntu/horovod/examples/mxnet_imagenet_resnet50.py"
+    echo "=====Start $1GPU $2 $3 $4 $5 $6====="
+    echo "$MPIRUN $ENV $CMD $OPTIONS"
+    TIC=$(date +%s)
+    $MPIRUN $ENV $CMD $OPTIONS
+    TIC=$(date +%s)
+    echo "=====Done $1GPU $2 $3 $4 $5 $6 in $(($TOC - $TIC)) Seconds====="
+    sleep 10
+}
+
+if [ "$#" -ne 3]; then
+    echo "Usage: ./benchmark.sh <num_gpus> <model> <lr_mode>"
+    exit
+fi
+
+run_benchmark $1 $2 $3 "non-HA" "float32" "synthetic"
+run_benchmark $1 $2 $3 "non-HA" "float32" "imagenet"
+run_benchmark $1 $2 $3 "HA" "float32" "synthetic"
+run_benchmark $1 $2 $3 "HA" "float32" "imagenet"
+run_benchmark $1 $2 $3 "non-HA" "float16" "synthetic"
+run_benchmark $1 $2 $3 "non-HA" "float16" "imagenet"
+run_benchmark $1 $2 $3 "HA" "float16" "synthetic"
+run_benchmark $1 $2 $3 "HA" "float16" "imagenet"
---
+++ benchmark/parse_results.py
@@ -0,0 +1,49 @@
+import sys
+
+def parse(res_file, epoch_num, batch_num):
+    train_acc = []
+    val_acc = []
+    speed = []
+    epoch = "Epoch[%s]" % epoch_num
+    with open(res_file, "r") as inf:
+        for line in inf:
+            if "Done" in line:
+                print(line.replace("\n", ""))
+                print_result(train_acc, "Train Accuracy")
+                print_result(val_acc, "Validation Accuracy")
+                print_result(speed, "Throughput")
+                train_acc = []
+                val_acc = []
+                speed = []
+            if epoch not in line:
+                continue
+            if "Train" in line:
+                train_acc.append(float(line.replace("\n", "").split("=")[-1]))
+            if "Validation" in line:
+                val_acc.append(float(float(line.replace("\n", "").split("=")[-1])))
+            if "Batch" in line and batch_num not in line:
+                sd = float(line.split(":")[3].split(" ")[1])
+                if sd >= 100.0:
+                    speed.append(sd)
+    
+
+def print_result(result, label):
+    if len(result) > 0:
+        decimal = 2
+        if label != "Throughput":
+            decimal = 4
+        max_num = round(max(result), decimal)
+        min_num = round(min(result), decimal)
+        mean_num = round(sum(result) / len(result), decimal)
+        print(label + " : max=%s min =%s mean=%s" % (max_num, min_num, mean_num))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python parse_results.py <result_file> <epoch_num> <batch_num>")
+        sys.exit()
+
+    res_file = sys.argv[1]
+    epoch_num = sys.argv[2]
+    batch_num = sys.argv[3]
+    parse(res_file, epoch_num, batch_num)
