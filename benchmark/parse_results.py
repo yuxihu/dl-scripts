f020eb7f3964aa1300f36b18b9c67dc423b58b9d
Yuxi Hu <darrenyxhu@gmail.com>
2019-01-22 11:39:37 -0800

    parse validation accuracy and training time

--- benchmark/parse_results.py
+++ benchmark/parse_results.py
@@ -1,4 +1,6 @@
+import re
 import sys
+
 
 def parse(res_file, epoch_num, batch_num):
     train_acc = []
@@ -39,12 +41,63 @@ def print_result(result, label):
         print(label + " : max=%s min=%s mean=%s" % (max_num, min_num, mean_num))
 
 
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: python parse_results.py <result_file> <epoch_num> <batch_num>")
-        sys.exit()
+def parse_accuracy(acc_file):
+    with open(acc_file, "r") as inf:
+        accuracy = []
+        for line in inf:
+            if "Start" in line:
+                setting = line.split(": ")[1].split("=")[0]
+                print("=====%s=====" % setting)
+            if "Validation-accuracy" in line:
+                rank = int(re.search(r"Rank\[([0-9]+)\]", line).group(1))
+                acc = float(line.split("=")[1])
+                accuracy.append((rank, acc))
+            if "Done" in line:
+                if len(accuracy) > 0:
+                    accuracy.sort(key=lambda tup: tup[1])
+                    print("max=%s" % str(accuracy[-1]))
+                    print("min=%s" % str(accuracy[0]))
+                    print("")
+                    accuracy = []
 
-    res_file = sys.argv[1]
-    epoch_num = sys.argv[2]
-    batch_num = sys.argv[3]
-    parse(res_file, epoch_num, batch_num)
+
+def parse_time(time_file):
+    with open(time_file, "r") as inf:
+        train_time = []
+        epoch_time = []
+        cur_epoch = 0
+        for line in inf:
+            if "Start" in line:
+                print(line.replace("\n", ""))
+            if "Time cost" in line:
+                epoch = int(re.search(r"Epoch\[([0-9]+)\]", line).group(1))
+                tc = float(line.split("=")[1])
+                if epoch != cur_epoch:
+                    train_time.append((cur_epoch, round(sum(epoch_time) / len(epoch_time), 2)))
+                    cur_epoch = epoch
+                    epoch_time = []
+                epoch_time.append(tc)
+            if "Done" in line:
+                samples = 1281167
+                train_time.sort(key=lambda tup: tup[1])
+                print("max=%s" % str(train_time[-1]))
+                print("min=%s" % str(train_time[0]))
+                print("speed=%s" % str(round(samples / train_time[0][1], 2)))
+                print("")
+                train_time = []
+
+
+if __name__ == "__main__":
+    if sys.argv[1] == "acc":
+        parse_accuracy(sys.argv[2])
+    elif sys.argv[1] == "time":
+        parse_time(sys.argv[2])
+    else:
+        if len(sys.argv) != 4:
+            print("Usage: python parse_results.py <result_file> <epoch_num> <batch_num>")
+            sys.exit()
+
+        res_file = sys.argv[1]
+        epoch_num = sys.argv[2]
+        batch_num = sys.argv[3]
+        parse(res_file, epoch_num, batch_num)
