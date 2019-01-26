33389934f15138004629cffb2e6adda640b2cf7b
Yuxi Hu <darrenyxhu@gmail.com>
2019-01-26 08:10:05 -0800

    simplify benchmark script

--- benchmark/horovod_benchmark.sh
+++ benchmark/horovod_benchmark.sh
@@ -5,77 +5,66 @@ function run_benchmark() {
   NUM_HOSTS=$(expr $NUM_WORKERS / 8)
   MODEL=$2
   LR_MODE=$3
-  DATA_SRC=$4
-  MEASURE=$5
-  ALLREDUCE=$6
-  DTYPE=$7
-  BATCH_SIZE=$8
-  OPT=$9
-  WARM_EP=${10}
+  ALLREDUCE=$4
+  DTYPE=$5
+  BATCH_SIZE=$6
+  OPT=$7
+  WARM_EP=$8
 
   MPIRUN="mpirun -np $NUM_WORKERS --hostfile $HOME/${NUM_HOSTS}hosts --bind-to none --map-by slot -mca pml ob1 -mca btl ^openib"
-  ENV="-x NCCL_DEBUG=INFO -x NCCL_MIN_NRINGS=4 -x MXNET_USE_OPERATOR_TUNING=0 -x HOROVOD_STALL_CHECK_DISABLE=1"
+  ENV="-x NCCL_MIN_NRINGS=4 -x MXNET_USE_OPERATOR_TUNING=0 -x HOROVOD_STALL_CHECK_DISABLE=1"
   if [[ $ALLREDUCE = "ha" ]]; then
     ENV="$ENV -x HOROVOD_HIERARCHICAL_ALLREDUCE=1"
   fi
 
-  SAVE_FREQ=45
+  SAVE_FREQ=90
   if [[ $BATCH_SIZE = 128 ]]; then
     LR=0.05
     DT=2
   else
     LR=0.1
-    DT=4
+    DT=3
   fi
-  OPTIONS="--model $MODEL --dtype $DTYPE --batch-size $BATCH_SIZE --lr $LR --lr-mode $LR_MODE --save-frequency $SAVE_FREQ --data-nthreads $DT --last-gamma"
-  if [[ $MEASURE == "accuracy" ]]; then
-    OPTIONS="$OPTIONS --warmup-epochs $WARM_EP --num-epochs 91"
-  else
-    OPTIONS="$OPTIONS --warmup-epochs 0 --num-epochs 1"
-  fi
-
-  SLEEP_TIME=10
-  if [[ $DATA_SRC != "synthetic" ]]; then
-    OPTIONS="$OPTIONS --use-rec --rec-train /media/ramdisk/train-480px-q95.rec --rec-train-idx /media/ramdisk/train-480px-q95.idx --rec-val /media/ramdisk/val-480px-q95.rec --rec-val-idx /media/ramdisk/val-480px-q95.idx"
-    SLEEP_TIME=60
-  fi
+  OPTIONS="--model $MODEL --dtype $DTYPE --batch-size $BATCH_SIZE --lr $LR --lr-mode $LR_MODE --optimizer $OPT"
+  OPTIONS="$OPTIONS --save-frequency $SAVE_FREQ --data-nthreads $DT --warmup-epochs $WARM_EP --num-epochs 91 --last-gamma"
+  OPTIONS="$OPTIONS --use-rec --rec-train /media/ramdisk/train-480px-q95.rec --rec-train-idx /media/ramdisk/train-480px-q95.idx --rec-val /media/ramdisk/val-480px-q95.rec --rec-val-idx /media/ramdisk/val-480px-q95.idx"
 
   CMD="/home/ubuntu/.virtualenvs/mxnet/bin/python /home/ubuntu/horovod/examples/mxnet_imagenet_resnet50.py"
-  echo "=====Start ${NUM_WORKERS}GPU $MODEL $ALLREDUCE $DTYPE $DATA_SRC====="
+  echo "=====Start ${NUM_WORKERS}GPU $ALLREDUCE $DTYPE $BATCH_SIZE $OPT $WARM_EP====="
   echo "$MPIRUN $ENV $CMD $OPTIONS"
   TIC=$(date +%s)
   $MPIRUN $ENV $CMD $OPTIONS
   TOC=$(date +%s)
-  echo "=====Done ${NUM_WORKERS}GPU $MODEL $ALLREDUCE $DTYPE $DATA_SRC in $(($TOC - $TIC)) Seconds====="
+  echo "=====Done ${NUM_WORKERS}GPU $ALLREDUCE $DTYPE $BATCH_SIZE $OPT $WARM_EP in $(($TOC - $TIC)) Seconds====="
 
   if ls ${MODEL}* 1> /dev/null 2>&1; then
     SSH_CMD="cd ~/train && ./save_checkpoint.sh $MODEL $LR_MODE $DTYPE $BATCH_SIZE $ALLREDUCE $OPT $WARM_EP"
     run_ssh_cmd "${SSH_CMD}"
   fi
 
-  sleep $SLEEP_TIME
+  sleep 60
 }
 
 function start_benchmark() {
-  #run_benchmark $1 $2 $3 $4 $5 "non-ha" "float32" 128 "sgd" 10
-  #run_benchmark $1 $2 $3 $4 $5 "ha" "float32" 128 "sgd" 10
-  #run_benchmark $1 $2 $3 $4 $5 "non-ha" "float32" 128 "sgd" 5
-  #run_benchmark $1 $2 $3 $4 $5 "ha" "float32" 128 "sgd" 5
+  run_benchmark $1 $2 $3 "ha" "float16" 256 "sgd" 10
+  run_benchmark $1 $2 $3 "ha" "float16" 256 "sgd" 5
+  run_benchmark $1 $2 $3 "ha" "float16" 256 "nag" 10
+  run_benchmark $1 $2 $3 "ha" "float16" 256 "nag" 5
 
-  run_benchmark $1 $2 $3 $4 $5 "non-ha" "float32" 128 "nag" 10
-  run_benchmark $1 $2 $3 $4 $5 "ha" "float32" 128 "nag" 10
-  run_benchmark $1 $2 $3 $4 $5 "non-ha" "float32" 128 "nag" 5
-  run_benchmark $1 $2 $3 $4 $5 "ha" "float32" 128 "nag" 5
+  # run_benchmark $1 $2 $3 "ha" "float32" 128 "sgd" 10
+  # run_benchmark $1 $2 $3 "ha" "float32" 128 "sgd" 5
+  # run_benchmark $1 $2 $3 "ha" "float32" 128 "nag" 10
+  # run_benchmark $1 $2 $3 "ha" "float32" 128 "nag" 5
 
-  #run_benchmark $1 $2 $3 $4 $5 "non-ha" "float16" 256 "sgd" 10
-  #run_benchmark $1 $2 $3 $4 $5 "ha" "float16" 256 "sgd" 10
-  #run_benchmark $1 $2 $3 $4 $5 "non-ha" "float16" 256 "sgd" 5
-  #run_benchmark $1 $2 $3 $4 $5 "ha" "float16" 256 "sgd" 5
+  # run_benchmark $1 $2 $3 "non-ha" "float16" 256 "sgd" 10
+  # run_benchmark $1 $2 $3 "non-ha" "float16" 256 "sgd" 5
+  # run_benchmark $1 $2 $3 "non-ha" "float16" 256 "nag" 10
+  # run_benchmark $1 $2 $3 "non-ha" "float16" 256 "nag" 5
 
-  #run_benchmark $1 $2 $3 $4 $5 "non-ha" "float16" 256 "nag" 10
-  #run_benchmark $1 $2 $3 $4 $5 "ha" "float16" 256 "nag" 10
-  #run_benchmark $1 $2 $3 $4 $5 "non-ha" "float16" 256 "nag" 5
-  #run_benchmark $1 $2 $3 $4 $5 "ha" "float16" 256 "nag" 5
+  # run_benchmark $1 $2 $3 "non-ha" "float32" 128 "sgd" 10
+  # run_benchmark $1 $2 $3 "non-ha" "float32" 128 "sgd" 5
+  # run_benchmark $1 $2 $3 "non-ha" "float32" 128 "nag" 10
+  # run_benchmark $1 $2 $3 "non-ha" "float32" 128 "nag" 5
 }
 
 function run_ssh_cmd() {
@@ -91,10 +80,10 @@ function run_ssh_cmd() {
   ssh 172.31.4.198 "${CMD}"
 }
 
-if [ "$#" -ne 5 ]; then
-  echo "Usage: ./benchmark.sh <num_gpus> <model> <lr_mode> <data_src> <measure>"
-  echo "Example: ./benchmark.sh 8 resnet50_v1 poly synthetic speed"
+if [ "$#" -ne 3 ]; then
+  echo "Usage: ./benchmark.sh <num_gpus> <model> <lr_mode>"
+  echo "Example: ./benchmark.sh 8 resnet50_v1 poly"
   exit
 fi
 
-start_benchmark $1 $2 $3 $4 $5
+start_benchmark $1 $2 $3
